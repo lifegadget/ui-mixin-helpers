6af0a6e14efdb14bdc4255154766031eaa20257c
Ken Snyder <ken@ken.net>
2015-01-01 15:58:42 +0000

    namespaced addon

--- app/mixins/input-status.js
+++ app/mixins/input-status.js
@@ -1,23 +1,4 @@
 import Ember from 'ember';
-var typeOf = Ember.typeOf;
+import InputStatusMixin from 'ui-mixin-helpers/mixins/input-status';
 
-export default Ember.Mixin.create({
-	status: 'default',
-	statusIsDefault: Ember.computed.equal('status','default'),
-	statusClass: function() {
-		var status = this.get('status');
-		if (typeOf(status) === 'function') {
-			status = status(this); // resolve function call
-		}
-		if(['error','err'].contains(status)) {
-			return 'status-error';
-		} else if(['warn','warning'].contains(status)) {
-			return 'status-warning';
-		} else if(['success','ok'].contains(status)) {
-			return 'status-success';
-		} else {
-			return 'status-default';
-		}
-	}.property('status','value'), // we look at 'value' for function based statuses
-	statusVisualize: false
-});
+export default InputStatusMixin;
