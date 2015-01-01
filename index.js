4304a9c6fec997fa39bf4bc66f8343aa929e4c2c
Ken Snyder <ken@ken.net>
2015-01-01 17:20:33 +0000

    app.import of mixins

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ui-mixin-helpers'
+	name: 'ui-mixin-helpers',
+	included: function(app) {
+		app.import('ui-mixin-helpers/mixins/input-correction.js');
+		app.import('ui-mixin-helpers/mixins/input-status.js');
+		app.import('ui-mixin-helpers/mixins/ui-event-listener.js');
+	}
 };
