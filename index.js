e74a17361cf5768f16b415fef449e429560a7755
Ken Snyder <ken@ken.net>
2015-01-01 19:51:43 +0000

    Update index.js
    removed app.imports for now

--- index.js
+++ index.js
@@ -3,9 +3,4 @@
 
 module.exports = {
 	name: 'ui-mixin-helpers',
-	included: function(app) {
-		app.import('ui-mixin-helpers/mixins/input-correction.js');
-		app.import('ui-mixin-helpers/mixins/input-status.js');
-		app.import('ui-mixin-helpers/mixins/ui-event-listener.js');
-	}
 };
